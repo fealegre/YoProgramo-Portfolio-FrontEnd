ec7bfcf2db82c5861da2e1197280f93c934395a7
alegrefer <alegrefer@gmail.com>
2025-05-18 21:16:57 +0000

    Before Angular 19

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire/compat';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
@@ -26,34 +26,27 @@ import { environment } from 'src/environments/environment';
 import { VerificarCorreoComponent } from './componentes/verificar-correo/verificar-correo.component';
 import { HeaderBarComponent } from './componentes/header-bar/header-bar.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    AboutComponent,
-    WorksComponent,
-    ContactComponent,
-    EducationComponent,
-    SkillsComponent,
-    JobsComponent,
-    FooterComponent,
-    HomeComponent,
-    LoginComponent,
-    RegistrarComponent,
-    RecuperarComponent,
-    VerificarCorreoComponent,
-    HeaderBarComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    NgCircleProgressModule.forRoot(),
-    AppRoutingModule,
-    ReactiveFormsModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    BrowserAnimationsModule, // required animations module
-    ToastrModule.forRoot(), // ToastrModule added
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        AboutComponent,
+        WorksComponent,
+        ContactComponent,
+        EducationComponent,
+        SkillsComponent,
+        JobsComponent,
+        FooterComponent,
+        HomeComponent,
+        LoginComponent,
+        RegistrarComponent,
+        RecuperarComponent,
+        VerificarCorreoComponent,
+        HeaderBarComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        NgCircleProgressModule.forRoot(),
+        AppRoutingModule,
+        ReactiveFormsModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        BrowserAnimationsModule, // required animations module
+        ToastrModule.forRoot()], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
